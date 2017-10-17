517da497003674759f417c57f556f5416bc9a3b2
Yanyu Liang <yanyul@andrew.cmu.edu>
2017-10-17 15:41:51 -0500

    debuged trimming

--- strategy2/fastq/do_trim.sh
+++ strategy2/fastq/do_trim.sh
@@ -4,13 +4,14 @@ input2=$3
 output1=$4
 output2=$5
 trimp=$6
-
-if [ $trim -eq 'No' ]
+echo $1 $2 
+echo $3 $4 $5 $6
+if [ "$trim" == 'No' ]
 then
   echo "No trim need to be done"
   cp $input1 $output1
   cp $input2 $output2
 else
   echo "Do trim" $trim
-  $trim $input1 $output1 $input2 $output2 $trimp
+  $trim $input1 $input2 $output1 $output1.unpaired $output2 $output2.unpaired $trimp
 fi
