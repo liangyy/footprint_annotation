a311a57cb059605ed0626e1cac096c056d526597
Yanyu Liang <yanyul@andrew.cmu.edu>
2018-07-03 16:39:04 -0500

    updated

--- strategy3/gen_experiment.py
+++ strategy3/gen_experiment.py
@@ -2,6 +2,7 @@ import argparse
 parser = argparse.ArgumentParser(prog='gen_experiment.py', description='''
     Generate the content needed for experiment section in config of this module
     This script will grab all files in `--dir` with BAM extention
+    CAUTION: please always use absolute path in `--dir`
 ''')
 parser.add_argument('--dir')
 args = parser.parse_args()
@@ -11,11 +12,10 @@ import glob
 import ntpath
 import os
 
-pwd = subprocess.check_output('pwd').strip()
+# pwd = subprocess.check_output('pwd').strip()
 bams = glob.glob('{dir}/*.bam'.format(dir = args.dir))
 for bam in bams:
-    name = os.path.splitext(bam)[0]
+    name = os.path.splitext(ntpath.basename(bam))[0]
     out = '''  {name}:
-        bam: '{path}/{bam}'
-'''.format(path = pwd, bam = bam, name = name)
+    bam: "{bam}"'''.format(bam = bam, name = name)
     print(out)
